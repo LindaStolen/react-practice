12ad72a4840e0719e975acabb9990ec1e94cb7c2
Linda Ho <plusmeow218@gmail.com>
2020-01-17 10:36:09 +0800

    20200116

--- practice-2/src/Board.js
+++ practice-2/src/Board.js
@@ -9,35 +9,68 @@ class Board extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      squares: Array(9).fill(null) //儲存Board的狀態
+      squares: Array(9).fill(null), //儲存Board的狀態
+      goNext: true, //利用boolean值改變來決定下一位出手者是誰 *
     }
     this.handleClick = this.handleClick.bind(this);
   }
+
   handleClick(i){
-    const squares = this.state.squares.slice();
-    squares[i]='X';
-    this.setState({squares:squares})
+    const squares = this.state.squares.slice(); //淺copy
+    squares[i] = this.state.goNext ? "O" : "X" //寫前面的先出手
+    this.setState({
+      squares:squares, //事件發生前board的狀態
+      goNext: !this.state.goNext, //*
+    })
+    console.log(squares[i])
+    console.log(this.state.squares)
+    calWinner(squares);
   }
+ 
 
   render(){
-    const status = 'Next one: ginger'
-    const rows = [];
+    const status = 'Next one: ' + (this.state.goNext ? "O" : "X") //寫前面的先出手
+    const rows = []; //用 Array 儲存九宮格
     for(let i = 0; i < 9; i++){
       rows.push(
         <Square key={i} value={this.state.squares[i]} onClick={() => this.handleClick(i)}/>
       )
     }
+    
+    // const winnner = calWinner(this.state.squares);
+    // let status;
+    // if(winner){
+    //   status = 'Winner:' + 
+    // }
+    console.log(this.state.squares) //事件發生後board的狀態
     return(
       <div className="board">
         <div className="status">{status}</div>
         
         <div className="place">
-          { rows }
+          { rows } {/* 在這邊呼叫 */}
           
         </div>
       </div>
     );
   }
+}
+
+function calWinner(squares) {
+    const line = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for(let i = 0; i < line.length; i++){
+      const [a,c,b] = line[i]
+      console.log(squares[a,b,c]);
+    }
 }
 
 export default Board;
--- practice-2/src/Square.js
+++ practice-2/src/Square.js
@@ -1,6 +1,6 @@
 // import React, { Component } from 'react'
 import React from 'react';
-
+// --------------------------第一種寫法---------------------------------
 // export default class Message extends Component {
 //   render() {
 //     return (
@@ -10,28 +10,35 @@ import React from 'react';
 //     )
 //   }
 // }
-
-class Square extends React.Component {
+// --------------------------class Compontent---------------------------------
+// class Square extends React.Component {
   // constructor(props){
   //   super(props);
   //   this.state={
   //     value:null, //屬於Square的私人初始狀態
   //   }
   // }
-  render(){
-    return (
-      <button className="Square" onClick={() => { this.props.onClick() }}>
-        {this.props.value} 
-        {/* props是上一層的board傳進來的 
-        }*/}
-      </button>
-    );
-  }
+//   render(){
+//     return (
+//       <button className="Square" onClick={() => { this.props.onClick() }}>
+//         {this.props.value} 
+//         {/* props是上一層的board傳進來的 
+//         }*/}
+//       </button>
+//     );
+//   }
+// }
+// export default Square;
+
+// --------------------------function Compontent---------------------------------
+function Square(props){
+  return (
+    <button className="Square" onClick={ props.onClick }>
+                      {/* 事件綁定, 前面的參數事porps, 所以這邊原地的 this.props 替換成 props, 後面原本執行事件的 "()" 也要拿掉 */}
+      {props.value} 
+      {/* props是上一層的board傳進來的 
+      }*/}
+    </button>
+  );
 }
 export default Square;
-
-
-// function Message(){
-//   return <p>trytrytry</p>
-// }
-// export default Message;
